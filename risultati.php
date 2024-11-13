f43288d2d7348731f1ce61d5fca14f03de4a9ffc
Mattia Cugusi <m.cugusi287gmail.com>
2024-11-13 19:33:20 +0100

    esercizio completato

--- risultati.php
+++ risultati.php
@@ -10,6 +10,22 @@ session_start();
     <title>Document</title>
 </head>
 <body>
-    
+    <?php
+    echo "<h1>numero di volte che è stata inviata la form: " . $_SESSION["invio"] . "</h1>";
+    echo "<ul>";
+    if (isset($_SESSION["voti"]) && is_array($_SESSION["voti"])) {
+    $totale = 0;
+    for ($i=0; $i<count($_SESSION["voti"]); $i++){
+        echo "<li> " . $_SESSION["voti"][$i] . "</li>";
+        $voto = intval($_SESSION["voti"][$i]);
+        $totale = $totale + $voto;
+    }
+    echo "</ul>";
+
+    $media = $totale /count($_SESSION["voti"]);
+
+    echo "<p>La media delle recensioni è: " . $media . "</p>";
+}
+    ?>
 </body>
 </html>
